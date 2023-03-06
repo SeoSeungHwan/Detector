6a302f4b8f32417e471777d7a26740cd2750b209
서승환 <tmdghks3264@naver.com>
2023-03-06 17:06:46 +0900

    refactor. 필요없는 코드 삭제

--- feature/base/src/main/java/com/soft/base/BaseActivity.kt
+++ feature/base/src/main/java/com/soft/base/BaseActivity.kt
@@ -1,14 +1,10 @@
 package com.soft.base
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.launch
 
 abstract class BaseActivity<T : ViewDataBinding, R : BaseViewModel> : AppCompatActivity() {
 
@@ -25,28 +21,7 @@ abstract class BaseActivity<T : ViewDataBinding, R : BaseViewModel> : AppCompatA
 
     abstract fun initAfterBinding()
 
-    /**
-     * Exception을 처리할 SharedFlow
-     */
-    protected var exception: SharedFlow<Throwable>? = null
     private var toast: Toast? = null
-
-    init {
-        lifecycleScope.launchWhenStarted {
-            launch {
-                exception?.collect { exception ->
-                    showToastMessage(exception)
-                }
-            }
-
-            launch {
-                viewModel.errorEvent.collect { e ->
-                    showToastMessage(e)
-                    Log.e("Detector", "onStart: ${e}")
-                }
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,18 +36,5 @@ abstract class BaseActivity<T : ViewDataBinding, R : BaseViewModel> : AppCompatA
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-        toast?.cancel()
-    }
-
-    //Toast Message 관련 함수
-    protected fun showToastMessage(e: Throwable?) {
-        toast?.cancel()
-        toast = Toast.makeText(this, e?.message, Toast.LENGTH_SHORT)?.apply { show() }
-    }
-
-    // Toast Message 관련 함수
-    protected fun toastMessage(message: String) {
-        toast?.cancel()
-        toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)?.apply { show() }
     }
 }
